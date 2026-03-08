4d16bd0f1382a4335a428ee4fc91c19593602b9d
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-03-08 22:01:21 +1100

    chore: remove treefmt-nix

--- flake.nix
+++ flake.nix
@@ -1,44 +1,25 @@
 {
   description = "copier-mr-minimal dev shell";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs =
-    { self, nixpkgs, treefmt-nix }:
-    let
-      eachSystem = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = {nixpkgs}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        alejandra
+        bun
+        libxml2
+        prettypst
+        shfmt
+        treefmt
+        uv
+        yq-go
+        nil
+        nixd
+        nixfmt
+        nixpkgs-fmt
       ];
-    in
-    {
-      formatter = eachSystem (
-        system: treefmt-nix.lib.mkWrapper nixpkgs.legacyPackages.${system} ./treefmt.nix
-      );
-
-      devShells = eachSystem (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            packages = [
-              pkgs.bun
-              pkgs.prettypst
-              pkgs.uv
-              pkgs.yq-go
-              pkgs.nil
-              pkgs.nixd
-              (treefmt-nix.lib.mkWrapper pkgs ./treefmt.nix)
-            ];
-          };
-        }
-      );
     };
+  };
 }
