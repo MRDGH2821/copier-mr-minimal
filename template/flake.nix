85fceef1b9f363db3590ced0d837bddec0b1f15c
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-03-09 23:15:09 +1100

    feat: add questions & flake.nix

---
+++ template/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "copier-mr-minimal dev shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    formatter.${system} = pkgs.treefmt;
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        alejandra
+        bun
+        libxml2
+        nil
+        nixd
+        nixfmt
+        prettypst
+        shfmt
+        treefmt
+        uv
+        yq-go
+      ];
+    };
+  };
+}
