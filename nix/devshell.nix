2c0c25a83515c52b49a775ad10fef448ec8ad109
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-07-24 19:51:38 +1000

    chore: remove apm install command

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -6,10 +6,7 @@
   pre-commit-check = import ./checks/pre-commit-check.nix {inherit inputs pkgs;};
 in
   pkgs.mkShell {
-    shellHook = ''
-      ${pkgs.lib.getExe inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.apm} install
-      ${pre-commit-check.shellHook}
-    '';
+    inherit (pre-commit-check) shellHook;
 
     packages = with pkgs; [
       inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.antigravity-cli
